39f6bfe786a4b35aaff9f466f324e8498c28cd72
Samuel Ironkwe <127967090+SamIron0@users.noreply.github.com>
2023-12-30 01:44:02 +0000

    gg

---
+++ pages/books.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import Head from 'next/head'
+import Base from '../layouts/Base'
+import stripHtml from '../lib/strip-html'
+import categories from '../data/uses'
+
+export async function getStaticProps() {
+  const meta = {
+    title: 'Uses // Samuel Ironkwe',
+    description:
+      "I often get messages asking about specific pieces of <strong>software or hardware I use</strong>. This not a static page, it's a <strong>living document</strong> with everything that I'm using nowadays.",
+    tagline: 'Fiction. Self Improvement. .',
+    image: '/static/images/uses-bw.jpg',
+    primaryColor: 'yellow',
+    secondaryColor: 'pink',
+  }
+
+  return { props: meta }
+}
+
+function Books(props) {
+  const { title, description, image } = props
+
+  const renderAll = () => {
+    return categories.map((category, index) => {
+      return (
+        <div key={index}>
+          <ul>
+            {category.items.map((item, iIndex) => {
+              return (
+                <p>
+                  {item.title}
+                </p>
+              )
+            })}
+          </ul>
+        </div>
+      )
+    })
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta content={title} property="og:title" />
+        <meta content={stripHtml(description)} name="description" />
+        <meta content={stripHtml(description)} property="og:description" />
+        <meta content="https://zenorocha.com/uses" property="og:url" />
+        <meta content={`https://zenorocha.com${image}`} property="og:image" />
+      </Head>
+
+      <p dangerouslySetInnerHTML={{ __html: description }} />
+
+      {renderAll()}
+    </>
+  )
+}
+
+Uses.Layout = Base
+
+export default Uses
