9772425b98be52092a3d544525c3e71351b5e8e5
SamIron0 <samuelironkwec@gmail.com>
2024-06-15 21:30:20 -0500

    x

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
-import { styled } from '../stitches.config'
-import Head from 'next/head'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import ShortcutHome from '../components/ShortcutHome'
-import { PostMain, PostContent, PostContainer } from '../components/Post'
-import { Wrapper } from '../components/Wrapper'
-import { getPersonJsonLd } from '../lib/json-ld'
+import { styled } from "../stitches.config";
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import ShortcutHome from "../components/ShortcutHome";
+import { PostMain, PostContent, PostContainer } from "../components/Post";
+import { Wrapper } from "../components/Wrapper";
+import { getPersonJsonLd } from "../lib/json-ld";
 
 export async function getStaticProps() {
   return {
     props: {
-      title: 'Samuel Ironkwe',
-      description: 'Obsessed with developer experience',
-      image: '/static/images/smauel.jpg',
+      title: "Samuel Ironkwe",
+      description: "Obsessed with developer experience",
+      image: "/static/images/smauel.jpg",
     },
-  }
+  };
 }
 
 export default function Index(props) {
-  const { title, description, image } = props
+  const { title, description, image } = props;
 
   return (
     <Wrapper>
@@ -32,7 +32,7 @@ export default function Index(props) {
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
-            __html: JSON.stringify(getPersonJsonLd())
+            __html: JSON.stringify(getPersonJsonLd()),
           }}
           key="person-jsonld"
         />
@@ -45,8 +45,12 @@ export default function Index(props) {
             <div>
               <h1>{title}</h1>
               <p>
-                <strong>Software Engineer and Entrepreneur
-                </strong><br />Currently building <a href="https://fitpalai.com" target="blank">FitpalAI</a>
+                <strong>Software Engineer and Entrepreneur</strong>
+                <br />
+                Currently building{" "}
+                <a href="https://cruiseo.xyz" target="blank">
+                  Cruiseo
+                </a>
               </p>
               <ShortcutHome />
             </div>
@@ -55,12 +59,12 @@ export default function Index(props) {
       </Home>
       <Footer />
     </Wrapper>
-  )
+  );
 }
 
 const Home = styled(PostMain, {
-  alignItems: 'center',
-  display: 'flex',
-  margin: '0 auto',
-  '@bp2': { width: 800 },
-})
+  alignItems: "center",
+  display: "flex",
+  margin: "0 auto",
+  "@bp2": { width: 800 },
+});
