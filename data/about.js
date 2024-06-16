4e4b40908a046e3f2678cb55dda7e77be89045a6
SamIron0 <samuelironkwec@gmail.com>
2024-06-15 19:36:39 -0500

    x

--- data/about.js
+++ data/about.js
@@ -1,21 +1,16 @@
 const items = [
   {
-   jobTitle: 'Software Engineer',
-   company: 'Freelance',
+   jobTitle: 'Full Stack Engineer',
+   company: 'Health Universe',
    companyUrl: 'https://ironkwe.site',
-   startDate: '2021-01-01',
+   startDate: '2022-09-01',
+   endDate: '2024-02-31',
    location: 'Remote',
  },
- {
-   jobTitle: 'Founder',
-   company: 'Cruiseo',
-   companyUrl: 'https://cruiseo.xyz',
-   startDate: '2023-11-01',
-   location: 'Winnipeg, Manitoba',
- },
+
  {
    jobTitle: 'Software Developer Intern',
-   company: 'Prologue XR',
+   company: 'Prologue',
    companyUrl: 'https://www.prologuexr.com',
    startDate: '2020-05-01',
    endDate: '2020-08-31',
