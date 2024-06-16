3abcc9c860c42bc98057b4f6a5b576de51209be6
SamIron0 <samuelironkwec@gmail.com>
2024-06-15 19:09:08 -0500

    x

--- pages/[slug].js
+++ pages/[slug].js
@@ -63,16 +63,6 @@ export async function getStaticProps({ params }) {
 
     const content = await convertMarkdownToHtml(post.content || '')
 
-    const isProd = process.env.NODE_ENV === 'production'
-    const base = isProd ? 'https://ironkwe.site' : 'http://localhost:3000'
-
-    if (isProd) {
-      const viewsReq = await fetch(`${base}/api/views/${params.slug}`)
-      const viewsRes = await viewsReq.json()
-
-      post.views = new Intl.NumberFormat().format(viewsRes.views || 0)
-    }
-
     return {
       props: {
         ...post,
