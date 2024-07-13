730cc090a75e144a3b0cd6e2f8942c82ecd3766a
SamIron0 <samuelironkwec@gmail.com>
2024-07-12 22:42:32 -0500

    x

--- pages/projects.js
+++ pages/projects.js
@@ -21,7 +21,7 @@ export async function getStaticProps() {
 
 function Projects(props) {
   const renderFeatured = () => {
-    const featured = ["Fitpal AI","Cruiseo",  "Scrapify"];
+    const featured = ["Fitpal AI","Cruiseo"];
 
     return items
       .map((item) => {
