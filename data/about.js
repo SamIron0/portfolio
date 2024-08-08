ffe80a824b0741c0d7fb1acd065849812a01db2a
SamIron0 <samuelironkwec@gmail.com>
2024-08-08 17:51:12 -0600

    updated work experience

--- data/about.js
+++ data/about.js
@@ -3,9 +3,9 @@ const items = [
     jobTitle: "Teaching Assistant",
     company: "University of Manitoba",
     companyUrl: "https://www.umanitoba.ca",
-    startDate: "2020-09-01",
+    startDate: "2024-09-01",
     endDate: "2024-12-15",
-    location: "Remote",
+    location: "Winnipeg, MB",
   },
   {
     jobTitle: "Software Developer Intern",
