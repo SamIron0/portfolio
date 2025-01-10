40b0d195c3bac8e5a1cdb45648f54eec71d201ee
Samuel Ironkwe <127967090+SamIron0@users.noreply.github.com>
2025-01-10 12:48:53 +0000

    update work

--- data/about.js
+++ data/about.js
@@ -5,7 +5,7 @@ const items = [
     company: "Prodigy Education",
     companyUrl: "https://www.prodigygame.com/main-en/",
     startDate: "2023-05-01",
-    endDate: "2023-08-31",
+    endDate: "2023-12-31",
     location: "Remote",
   },
 ];
